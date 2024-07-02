ec1380047d742ea776ab974d690581a0e9378ab2
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-02 11:13:36 +0300

    Re-factor Graph

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from ui.containers.MyFrame import MyFrame
+from models.Graph import Graph
 
 
-my_frame = MyFrame(width=800, height=500, bg="red", title="Flow Max")
-my_frame.mainloop()
+# my_frame = MyFrame(width=800, height=500, bg="red", title="Flow Max")
+# my_frame.mainloop()
+
+my_graph = Graph(head_title="S", tail_title="P")
+my_graph.initialize_data()
+
+my_graph.get_unvisited_neighbors('A')
+
+print("Hi")
