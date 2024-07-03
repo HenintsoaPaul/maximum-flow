0ccfccddf5348938a246fb48d8c96147ce716b05
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-03 13:49:12 +0300

    Add properties in FlowState.py

--- models/FlowState.py
+++ models/FlowState.py
@@ -1,7 +1,17 @@
 class FlowState:
-    def __init__(self, plus: bool, potential: int) -> None:
+    def __init__(self, start: str, end: str, plus: bool, potential: int) -> None:
+        self._start: str = start
+        self._end: str = end
         self._plus: bool = plus
         self._potential: int = potential
+        
+    @property
+    def start(self) -> str:
+        return self._start
+    
+    @property
+    def end(self) -> str:
+        return self._end
         
     @property
     def plus(self) -> bool:
